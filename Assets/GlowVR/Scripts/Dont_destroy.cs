81c55eb734d68579428e84d0130d4286802ef118
TheFalcon98 <thefalcon981@gmail.com>
2026-02-18 11:05:05 +0100

    .

---
+++ Assets/GlowVR/Scripts/Dont_destroy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dont_destroy : MonoBehaviour {
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+
+
+    }
+}
